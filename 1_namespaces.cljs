f64cfb05a6bd8c1aecb752698ae07a564701ca68
Mawuli Adzoe <mawuli.kofi.mawuli@gmail.com>
2015-09-20 10:54:01 +0000

    Add namespaces. Separate file.

---
+++ 1_namespaces.cljs
@@ -0,0 +1,33 @@
+;; -=-=--=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-==--=
+;; An Introduction to ClojureScript for Light Table users
+;; -==-=--=-=-=-=-=-=-=-=-==-=--=-=-==--==-=-=-=-=--===--==-
+
+
+;; Basic
+;; -==-=-=-=--=-=-=-=-=-=--=-=-==-=-=--==--=-=-=-=-=-=-=-==-
+
+;; To begin, open the command pane (type Control-SPACE), Add Connection, select
+;; Light Table UI. Once connected you can evaluate tall the forms in this file
+;; by placeing the cursor after the form and typing Command-ENTER.
+
+;; IMPORTANT: You must evaluate the very first form, the namespace definition.
+
+;; Declaring namespaces
+;; -=-==--=-=-=-=-=-==--==--=-=-=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+;; ClojureScript supports modularity via namespaces. They allow you to group
+;; logical definitions together.
+
+(ns lt-cljs-tutorial
+  (:require [clojure.string :as string]))
+
+;; :require is how you can import functionality from a different namespace into
+;; the current one. Here we are requiring `clojure.string` and giving it an
+;;alias. We could write the following:
+
+(clojure.string/blank? "")
+
+;; but thats really verbose compared to:
+
+(string/blank? "")
+
