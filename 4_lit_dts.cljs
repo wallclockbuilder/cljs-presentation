9d5a2a01b9c8a19f46c24abea16ad89d8a77aba8
Mawuli Adzoe <mawuli.kofi.mawuli@gmail.com>
2015-09-21 05:15:58 +0000

    Add literal data types

---
+++ 4_lit_dts.cljs
@@ -0,0 +1,32 @@
+;; -=-=--=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-==--=
+;; An Introduction to ClojureScript for Light Table users
+;; -==-=--=-=-=-=-=-=-=-=-==-=--=-=-==--==-=-=-=-=--===--==-
+
+(ns lt-cljs-tutorial
+  (:require [clojure.string :as string]))
+
+;; Literal data types
+;; -==--=-=-=-=-=-=-=-=
+
+;; ClojureScript comes out of the box with the usual useful data literals.
+
+;; Booleans
+
+(def a-boolean true)
+
+;; Strings
+
+(def a-string "Hello!")
+
+;; Regular Expressions
+
+(def a-regexp #"\d{3}-?\d{3}-?d{4}")
+
+;; Numbers
+
+(def a-number 1)
+
+
+
+
+
