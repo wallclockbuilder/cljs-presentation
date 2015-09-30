bee24d3da4f978b1ed4e585839128422b53c79e5
Mawuli Adzoe <mawuli.kofi.mawuli@gmail.com>
2015-09-30 05:42:12 +0000

    Add cond

---
+++ 16_cond/16_cond.cljs
@@ -0,0 +1,17 @@
+;; -=-=--=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-==--=
+;; An Introduction to ClojureScript for Light Table users
+;; -==-=--=-=-=-=-=-=-=-=-==-=--=-=-==--==-=-=-=-=--===--==-
+
+(ns lt-cljs-tutorial
+  (:require [clojure.string :as string]))
+
+
+;; cond
+;; --=-
+
+;; Nesting `if` tends to be noisy and hard to read so ClojureScript provides a `cond` macro to deal with this.
+
+(cond
+ nil "Not goint to return this"
+ false "Nope not going to return this either"
+ :else "Default case")
