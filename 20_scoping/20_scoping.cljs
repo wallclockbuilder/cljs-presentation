fd4e12e6c65f291c957870adc520873e38874d31
Mawuli Adzoe <mawuli.kofi.mawuli@gmail.com>
2015-10-01 09:29:36 +0000

    Wrap lines.

--- 20_scoping/20_scoping.cljs
+++ 20_scoping/20_scoping.cljs
@@ -26,7 +26,9 @@ some-x
 ;; Closures
 ;; --------
 
-;; Could a language with such a name miss closures? Surely it can't. You may be already familiar with them in JavaScript, even if it's a varialbe scoped language.
+;; Could a language with such a name miss closures? Surely it can't.
+;; You may be already familiar with them in JavaScript, even if it's a
+;; variable scoped language.
 
 
 (let [a 1e3]
@@ -36,7 +38,11 @@ some-x
     (+ (foo) a)))
 
 
-;; Above we defined `foo` and `bar` functions inside the scope of a `let` form and they both know about `a` (i.e. they close over `a`) Note, even if defined inside a `let`, `foo` and `bar` are available in the outer scope. This is because all `def` expressions are always top level. See the footnote at the end of this section.
+;; Above we defined `foo` and `bar` functions inside the scope of a
+;; `let` form and they both know about `a` (i.e. they close over `a`)
+;; Note, even if defined inside a `let`, `foo` and `bar` are available
+;; in the outer scope. This is because all `def` expressions are always
+;; top level. See the footnote at the end of this section.
 
 
 (foo)
@@ -54,7 +60,8 @@ some-x
 ;; That's why some people say that closures are the poor man's objects.
 ;; They encapsulate the information as well.
 
-;; in ClojureScript, functions' parameters and let bindings' locals are not mutable! That goges for loop locals too!
+;; But in ClojureScript, functions' parameters and let bindings' locals
+;; are not mutable! That goes for loop locals too!
 
 
 (let [fns (loop [i 0 ret []]
@@ -64,17 +71,20 @@ some-x
   (map #(%) fns))
 
 
-;; In JavaScript you would see a list of ten 9s. In ClojureScript we see the expected numbers from 0 to 9.
+;; In JavaScript you would see a list of ten 9s. In ClojureScript we
+;; see the expected numbers from 0 to 9.
 
 ;; FOOTNOTE:
 ;;
-;; `def` expressions (including `defn`) are always top level. People familiar with Scheme or other Lisps often mistakenly write the following in Clojure:
+;; `def` expressions (including `defn`) are always top level. People familiar
+;; with Scheme or other Lisps often mistakenly write the following in Clojure:
 
 
 (defn not-scheme []
   (defn no-no-no []))
 
-;; This is almost always incorrect. If you need to write a local function just do it with a let binding.
+;; This is almost always incorrect. If you need to write a local function
+;; just do it with a let binding.
 
 
 (defn outer-fn []
