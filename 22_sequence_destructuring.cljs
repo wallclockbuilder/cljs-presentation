b53af0eeb3ca98f639c31b7c83c821d71c4ac815
Mawueli Kofi Adzoe <wallclockbuilder@googlemail.com>
2015-10-05 01:23:25 +0000

    Create 22_sequence_destructuring.cljs

---
+++ 22_sequence_destructuring.cljs
@@ -0,0 +1,39 @@
+;; -=-=--=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-==--=
+;; An Introduction to ClojureScript for Light Table users
+;; -==-=--=-=-=-=-=-=-=-=-==-=--=-=-==--==-=-=-=-=--===--==-
+
+(ns lt-cljs-tutorial
+  (:require [clojure.string :as string]))
+
+
+;; Sequence destructuring
+;; ----------------------
+
+
+;; Destructuring sequential types is particularly useful.
+
+(let [[f & r] '(1 2 3)]
+  f)
+
+
+(let [[f & r] '(1 2 3)]
+  r)
+
+
+(let [[r g b] [255 255 150]]
+  g)
+
+
+;; _ is just a convention for saying that you are not intrested in the item at the corresponding position. It has not other special meaning.
+;; Here we're only interested inthe third local barialbe named 'b'.
+
+
+(let [[_ _ b] [255 255 150]]
+  b)
+
+
+;; destructuring function arguments works just as well. Here we are only interested in the second argument `g`.
+
+(defn green [[_ g _]] g)
+
+(green [255 255 150])
