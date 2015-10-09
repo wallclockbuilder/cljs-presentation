9cf852c43b6cb4082e74e72a3246ee925ad1a4f8
Mawuli Adzoe <mawuli.kofi.mawuli@gmail.com>
2015-10-09 09:11:06 +0000

    Fix line wrapping. Fix typos.

--- 23_sequence_destructuring/23_sequence_destructuring.cljs
+++ 23_sequence_destructuring/23_sequence_destructuring.cljs
@@ -24,15 +24,17 @@
   g)
 
 
-;; _ is just a convention for saying that you are not intrested in the item at the corresponding position. It has not other special meaning.
-;; Here we're only interested inthe third local barialbe named 'b'.
+;; _ is just a convention for saying that you are not intrested in the
+;; item at the corresponding position. It has no other special meaning.
+;; Here we're only interested in the third local variable named 'b'.
 
 
 (let [[_ _ b] [255 255 150]]
   b)
 
 
-;; destructuring function arguments works just as well. Here we are only interested in the second argument `g`.
+;; destructuring function arguments works just as well. Here we are
+;; only interested in the second argument `g`.
 
 (defn green [[_ g _]] g)
 
